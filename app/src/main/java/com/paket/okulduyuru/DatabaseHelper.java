fa493f6e21913ab1cdca28e969571c66fb7cb1b3
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-01-21 17:21:25 +0300

    Home arayüzü düzenlendi.

--- app/src/main/java/com/paket/okulduyuru/DatabaseHelper.java
+++ app/src/main/java/com/paket/okulduyuru/DatabaseHelper.java
@@ -76,5 +76,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-
 }
--- app/src/main/java/com/paket/okulduyuru/HomeActivity.java
+++ app/src/main/java/com/paket/okulduyuru/HomeActivity.java
@@ -2,7 +2,9 @@ package com.paket.okulduyuru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
