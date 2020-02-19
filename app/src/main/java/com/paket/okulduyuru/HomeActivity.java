d12eb6fe5fd2d7d5d7893224cd3debfbdefb092b
Berk Şenman <berk_senman_1999@hotmail.com>
2020-02-19 03:03:24 +0300

    Arel Web yeni düzeltme yapıldı

--- app/src/main/java/com/paket/okulduyuru/HomeActivity.java
+++ app/src/main/java/com/paket/okulduyuru/HomeActivity.java
@@ -2,29 +2,48 @@ package com.paket.okulduyuru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     DatabaseHelper db;
-    TextView txtAdSoyad;
+    TextView txtAdSoyad,txtArelWeb;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
         txtAdSoyad = findViewById(R.id.adSoyad);
+        txtArelWeb = findViewById(R.id.txtArelWeb);
         db = new DatabaseHelper(this);
         //Ad Ve Soyadı veritabanından çekiyoruz.
         Cursor cursor = db.verilereUlas();
         while (cursor.moveToNext()) {
             txtAdSoyad.setText(cursor.getString(1));
         }
+
+        txtArelWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this,ArelWebView.class);
+                startActivity(intent);
+            }
+        });
+
+    }
     }
 
 
-}
+
 
