28a9ebd556b75aacc3f7e7e6d7c46f455ab3556e
Berk Şenman <berk_senman_1999@hotmail.com>
2020-02-22 01:13:48 +0300

    Home arayüzü düzenlenecek

--- app/src/main/java/com/paket/okulduyuru/ArelWebView.java
+++ app/src/main/java/com/paket/okulduyuru/ArelWebView.java
@@ -7,33 +7,20 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class ArelWebView extends AppCompatActivity {
-<<<<<<< HEAD
-    WebView webView;
-=======
 
     WebView webView;
 
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arel_web_view);
 
-<<<<<<< HEAD
+
+
         webView=(WebView)findViewById(R.id.webView);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.arel.edu.tr");
-
-
-=======
-
-
-        webView=(WebView)findViewById(R.id.webView);
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.arel.edu.tr/");
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
     }
 }
--- app/src/main/java/com/paket/okulduyuru/HomeActivity.java
+++ app/src/main/java/com/paket/okulduyuru/HomeActivity.java
@@ -7,24 +7,23 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
+
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-=======
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     DatabaseHelper db;
-<<<<<<< HEAD
-    TextView txtAdSoyad,txtArelWeb;
+
+    TextView txtAdSoyad,txtArelWeb,txtDuyurular;
 
 
-=======
-    TextView txtAdSoyad,txtDuyurular,txtArelWeb;
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +31,13 @@ public class HomeActivity extends AppCompatActivity {
 
 
         txtAdSoyad = findViewById(R.id.adSoyad);
-<<<<<<< HEAD
+
         txtArelWeb = findViewById(R.id.txtArelWeb);
-=======
+
         txtDuyurular = findViewById(R.id.txtDuyurular);
         txtArelWeb = findViewById(R.id.txtArelWeb);
 
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
+
         db = new DatabaseHelper(this);
         //Ad Ve Soyadı veritabanından çekiyoruz.
         Cursor cursor = db.verilereUlas();
@@ -46,8 +45,7 @@ public class HomeActivity extends AppCompatActivity {
             txtAdSoyad.setText(cursor.getString(1));
         }
 
-<<<<<<< HEAD
-=======
+
         //Duyurular sayfasına geçiş..
         txtDuyurular.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +56,6 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         //Arel Web sayfasına geçiş.
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
         txtArelWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,16 +64,16 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-<<<<<<< HEAD
+
     }
     }
-=======
 
 
-            }
 
 
->>>>>>> 37306d8928ea673591e0c7788e6701cb0a8158a1
+
+
+
 
 
 
