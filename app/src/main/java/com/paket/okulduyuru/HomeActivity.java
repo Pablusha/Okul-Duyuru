8226532af303443d35bf569741102fb1303f506c
Berk Şenman <berk_senman_1999@hotmail.com>
2020-02-22 04:08:04 +0300

    Login ve Register arayüzleri düzenlendi.

--- app/src/main/java/com/paket/okulduyuru/HomeActivity.java
+++ app/src/main/java/com/paket/okulduyuru/HomeActivity.java
@@ -7,49 +7,31 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
+
 
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-=======
+
 import android.widget.RelativeLayout;
->>>>>>> ac3e0705d906ff9a7184646aa93c7ee47dae60bb
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     DatabaseHelper db;
-<<<<<<< HEAD
-
     TextView txtAdSoyad,txtArelWeb,txtDuyurular;
 
-
-
-
-
-=======
-    TextView txtAdSoyad,txtArelWeb;
-    RelativeLayout txtDuyurular;
->>>>>>> ac3e0705d906ff9a7184646aa93c7ee47dae60bb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
         txtAdSoyad = findViewById(R.id.adSoyad);
-<<<<<<< HEAD
-
         txtArelWeb = findViewById(R.id.txtArelWeb);
-
-        txtDuyurular = findViewById(R.id.txtDuyurular);
-=======
-        txtDuyurular = findViewById(R.id.ac_home_cv_duyurular);
->>>>>>> ac3e0705d906ff9a7184646aa93c7ee47dae60bb
+        txtDuyurular = findViewById(R.id.ac_home_tv_duyurular);
         txtArelWeb = findViewById(R.id.txtArelWeb);
-
 
         db = new DatabaseHelper(this);
         //Ad Ve Soyadı veritabanından çekiyoruz.
@@ -57,7 +39,6 @@ public class HomeActivity extends AppCompatActivity {
         while (cursor.moveToNext()) {
             txtAdSoyad.setText(cursor.getString(1));
         }
-
 
         //Duyurular sayfasına geçiş..
         txtDuyurular.setOnClickListener(new View.OnClickListener() {
@@ -80,14 +61,4 @@ public class HomeActivity extends AppCompatActivity {
 
     }
     }
-
-
-
-
-
-
-
-
-
-
 
