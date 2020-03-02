926a629cbee84cb2ee242430a1b3af5086e3d90c
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-03-02 15:44:51 +0300

    Öğretmenlerin admin paneli hazırlanmaya başlandı.

--- app/src/main/java/com/paket/okulduyuru/utils/Sabit.java
+++ app/src/main/java/com/paket/okulduyuru/utils/Sabit.java
@@ -1,7 +1,11 @@
 package com.paket.okulduyuru.utils;
 
+import com.paket.okulduyuru.Ogrenci;
+
 public class Sabit {
 
-  public static String KEY_EMAİL = "email";
-  public static String KEY_SIFRE = "sifre";
+  private static Ogrenci onlineOgrenci;
+
+  public static final String KEY_EMAİL = "email";
+  public static final String KEY_SIFRE = "sifre";
 }
