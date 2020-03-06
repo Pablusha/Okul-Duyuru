5d9e5673cf4d94af16e5bf7395686897aee27f1b
Berk Şenman <berk_senman_1999@hotmail.com>
2020-03-06 12:56:00 +0300

    Dosyalar çekildi

---
+++ app/src/main/java/com/paket/okulduyuru/activitiy_ogretmen_duyuru_add.java
@@ -0,0 +1,14 @@
+package com.paket.okulduyuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activitiy_ogretmen_duyuru_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitiy_ogretmen_duyuru_add);
+    }
+}
