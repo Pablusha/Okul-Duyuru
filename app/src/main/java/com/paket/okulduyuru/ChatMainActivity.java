f5610ebcf9ec096654e2d3460c5d379fc4bf827a
Berk Şenman <berk_senman_1999@hotmail.com>
2020-03-07 18:51:50 +0300

    Mesajlaşma ilk adım atıldı!

---
+++ app/src/main/java/com/paket/okulduyuru/ChatMainActivity.java
@@ -0,0 +1,35 @@
+package com.paket.okulduyuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ChatMainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        mToolbar.setSubtitle("Mesajlaşma");
+
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+}
