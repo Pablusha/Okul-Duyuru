576520fa1c8ad7bc41dc21228213f27f78b18349
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-03-09 13:00:20 +0300

    Öğretmen duyuru yönetim sistemi düzenlenmeye başlandı.

--- app/src/main/java/com/paket/okulduyuru/LoginActivity.java
+++ app/src/main/java/com/paket/okulduyuru/LoginActivity.java
@@ -4,11 +4,13 @@ package com.paket.okulduyuru;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,18 +18,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.SignInMethodQueryResult;
-import com.paket.okulduyuru.utils.PreferenceUtils;
-import com.paket.okulduyuru.utils.Sabit;
-
-import io.paperdb.Paper;
-
 
 public class LoginActivity extends AppCompatActivity {
     private EditText edEmail;
     private EditText edSifre;
     private Button btnGiris,btnOgretmenGiris;
     private FirebaseAuth firebaseAuth;
-    private String parentDbName = "Ogrenci";
     private CheckBox chkBeniHatirla;
 
     @Override
@@ -40,19 +36,8 @@ public class LoginActivity extends AppCompatActivity {
         btnGiris = findViewById(R.id.btnGiris);
         btnOgretmenGiris = findViewById(R.id.btnOgretmenGiris);
         chkBeniHatirla = findViewById(R.id.ac_ogrenci_login_chk_beni_hatirla);
-        Paper.init(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
-
-        try {
-            String email = edEmail.getText().toString();
-            String sifre = edSifre.getText().toString();
-            if (chkBeniHatirla.isChecked()) {
-                Paper.book().write(Sabit.KEY_EMAIL,email);
-                Paper.book().write(Sabit.KEY_SIFRE,sifre);
-            }
-        } catch (Exception e) {
-        }
 
         btnGiris.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,9 +76,6 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
 
     }
 
