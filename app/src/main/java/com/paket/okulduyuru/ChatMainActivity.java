6a8ba5c153190dc1bcfc8a14092ad0f7113f368d
Berk Şenman <berk_senman_1999@hotmail.com>
2020-05-28 19:23:03 +0300

    Mesajlaşma için grup oluşturma tamamlandı!

--- app/src/main/java/com/paket/okulduyuru/ChatMainActivity.java
+++ app/src/main/java/com/paket/okulduyuru/ChatMainActivity.java
@@ -2,19 +2,25 @@ package com.paket.okulduyuru;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,6 +29,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.paket.okulduyuru.Model.Ogretmen;
 import com.paket.okulduyuru.UI.LoginActivity;
 
 public class ChatMainActivity extends AppCompatActivity {
@@ -54,6 +61,54 @@ public class ChatMainActivity extends AppCompatActivity {
         myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
 
+    }
+
+    private void RequestNewGroup() {
+        AlertDialog.Builder builder  = new AlertDialog.Builder(ChatMainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Grup Adı Belirleyin :");
+
+        final EditText groupNameField = new EditText(ChatMainActivity.this);
+        groupNameField.setHint("Orn: Bilgisayar Programcılığı");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Oluştur", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupName = groupNameField.getText().toString();
+
+                if(TextUtils.isEmpty(groupName))
+                {
+                    Toast.makeText(ChatMainActivity.this, "Lütfen Bir Grup Adı Giriniz!..", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+    }
+
+    private void CreateNewGroup(final String groupName) {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(ChatMainActivity.this, groupName + " Grubu Oluşturuldu!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void SendUserToSettingsActivity() {
@@ -121,6 +176,7 @@ public class ChatMainActivity extends AppCompatActivity {
 
         super.onOptionsItemSelected(item);
 
+
         if (item.getItemId() == R.id.main_logout_option) {
             firebaseAuth.signOut();
             SendUserToLoginActivity();
@@ -129,10 +185,15 @@ public class ChatMainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.main_settings_option) {
             SendUserToSettingsActivity();
         }
+        if (item.getItemId() == R.id.main_create_group_option) {
+            RequestNewGroup();
+        }
         if (item.getItemId() == R.id.main_find_friends_option) {
 
         }
         return true;
     }
+
+
 
 }
