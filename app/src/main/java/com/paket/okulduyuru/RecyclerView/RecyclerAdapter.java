3c65e494c1670810e885f9177c8ed684b7fe5a3b
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-05-31 17:45:55 +0300

    Duyuru güncelleme ekranı geliştiriliyor.

--- app/src/main/java/com/paket/okulduyuru/RecyclerView/RecyclerAdapter.java
+++ app/src/main/java/com/paket/okulduyuru/RecyclerView/RecyclerAdapter.java
@@ -1,9 +1,11 @@
 package com.paket.okulduyuru.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.paket.okulduyuru.Model.Duyuru;
 import com.paket.okulduyuru.R;
+import com.paket.okulduyuru.UI.activity_duyuru_update_screen;
 
 import java.util.ArrayList;
 
@@ -35,12 +38,27 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.txtBaslik.setText(duyuruArrayList.get(position).getDuyuruBaslik());
         holder.txtContext.setText(duyuruArrayList.get(position).getDuyuruContext());
         holder.txtYazar.setText(duyuruArrayList.get(position).getDuyuruYazar());
         holder.txtTime.setText(duyuruArrayList.get(position).getDuyuruTime());
         holder.txtDate.setText(duyuruArrayList.get(position).getDuyuruDate());
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, activity_duyuru_update_screen.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("baslik",duyuruArrayList.get(position).getDuyuruBaslik());
+                intent.putExtra("context",duyuruArrayList.get(position).getDuyuruContext());
+                intent.putExtra("yazar",duyuruArrayList.get(position).getDuyuruYazar());
+                intent.putExtra("time",duyuruArrayList.get(position).getDuyuruTime());
+                intent.putExtra("date",duyuruArrayList.get(position).getDuyuruDate());
+                mContext.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
@@ -51,6 +69,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtBaslik,txtContext,txtYazar,txtTime,txtDate;
+        RelativeLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -60,6 +79,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             txtTime = itemView.findViewById(R.id.ac_cv_duyuru_time);
             txtYazar = itemView.findViewById(R.id.ac_cv_duyuru_yazar);
             txtDate = itemView.findViewById(R.id.ac_cv_duyuru_date);
+            parentLayout = itemView.findViewById(R.id.ac_items_layout);
         }
     }
 
--- app/src/main/java/com/paket/okulduyuru/UI/activity_duyuru_update_screen.java
+++ app/src/main/java/com/paket/okulduyuru/UI/activity_duyuru_update_screen.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.paket.okulduyuru.R;
 
 public class activity_duyuru_update_screen extends AppCompatActivity {
 
     private Spinner bolumler;
+    DatabaseReference reference;
+    String baslik,context,yazar,date,time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duyuru_update_screen);
+
+        reference = FirebaseDatabase.getInstance().getReference("Duyuru");
+
 
         bolumler = findViewById(R.id.ac_ogretmen_duyuru_update_spinner);
 
@@ -23,6 +33,49 @@ public class activity_duyuru_update_screen extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.bolumler));
         bolumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         bolumler.setAdapter(bolumAdapter);
+
+        getComingIntent();
+
+    }
+
+    private void sendUpdatesToFirebase() {
+        if (isBaslikChanged() || isContextChanged()) {
+            Toast.makeText(activity_duyuru_update_screen.this,"Güncelleme başarılı.",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isContextChanged() {
+        return true;
+    }
+
+    private boolean isBaslikChanged() {
+        EditText duyuruBaslik = findViewById(R.id.ac_ogretmen_duyuru_update_title);
+        if (baslik.equals(duyuruBaslik.getText().toString())) {
+
+        }
+        return true;
+    }
+
+    private void getComingIntent() {
+        if (getIntent().hasExtra("baslik") && getIntent().hasExtra("context") && getIntent().hasExtra("yazar") && getIntent().hasExtra("time")
+        && getIntent().hasExtra("date")) {
+            String baslik = getIntent().getStringExtra("baslik");
+            String context = getIntent().getStringExtra("context");
+            String yazar = getIntent().getStringExtra("yazar");
+            String time = getIntent().getStringExtra("time");
+            String date = getIntent().getStringExtra("date");
+
+            setDatas(baslik,context,yazar,time,date);
+        }
+    }
+
+    private void setDatas(String baslik,String context,String yazar,String time,String date) {
+        EditText duyuruBaslik = findViewById(R.id.ac_ogretmen_duyuru_update_title);
+        duyuruBaslik.setText(baslik);
+        EditText duyuruContext = findViewById(R.id.ac_ogretmen_duyuru_update_context);
+        duyuruContext.setText(context);
+        EditText duyuruYazar = findViewById(R.id.ac_ogretmen_duyuru_update_author);
+        duyuruYazar.setText(yazar);
 
     }
 }
